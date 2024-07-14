41ef78f304346ca0eebbdba602686946d6b4b852
interacsean <seanhasselback@gmail.com>
2024-07-15 07:07:07 +1000

    Size canvas for screen

---
+++ src/components/Wheel/useFullScreenCanvas.ts
@@ -0,0 +1,27 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+
+export function useFullScreenCanvas(
+  canvasRef: MutableRefObject<HTMLCanvasElement | null>,
+  redraw: () => void,
+) {
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const setCanvasSize = () => {
+      console.log('Resized');
+      if (canvas) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        redraw();
+      }
+    };
+
+    setCanvasSize();
+    window.addEventListener('resize', setCanvasSize);
+
+    return () => {
+      window.removeEventListener('resize', setCanvasSize);
+    };
+  }, [redraw]);
+
+  return canvasRef;
+}
